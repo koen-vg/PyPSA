1a7d6a5202b602074a222286bd3c66d1daea8ff8
Koen van Greevenbroek <koenvg@stanford.edu>
2025-09-17 15:36:00 -0700

    Improve model creation performance; replace np.isin by pandas

--- pypsa/optimization/constraints.py
+++ pypsa/optimization/constraints.py
@@ -936,14 +936,19 @@ def define_nodal_balance_constraints(
         # Only keep the first scenario if there are multiple
         if n.has_scenarios:
             cbuses = cbuses.isel(scenario=0, drop=True)
-        cbuses = cbuses[cbuses.isin(buses)].rename("Bus")
 
-        if not cbuses.size:
+        # `numpy.isin` on object arrays scales poorly; use pandas hashing instead
+        cbuses_data = cbuses.data
+        mask = pd.Index(cbuses_data).isin(buses)
+
+        # drop non-existent multiport buses which are ''
+        if column in ["bus" + i for i in n.c.links.additional_ports]:
+            mask &= cbuses_data != ""
+
+        if not mask.any():
             continue
 
-        #  drop non-existent multiport buses which are ''
-        if column in ["bus" + i for i in n.c.links.additional_ports]:
-            cbuses = cbuses[cbuses != ""]
+        cbuses = cbuses[mask].rename("Bus")
 
         expr = expr.sel(name=cbuses.coords["name"].values)
         if expr.size:
